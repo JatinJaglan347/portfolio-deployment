25e14d9d7a95f648ca9437adf3d15837aa1db7a2
JatinJaglan347 <jatinjaglan347@gmail.comm>
2024-03-20 11:41:24 +0530

    third

--- src/main.jsx
+++ src/main.jsx
@@ -43,11 +43,5 @@ const router =createBrowserRouter(
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    {/* <App /> */}
-    {/* <Home /> */}
-    {/* <GithubRepo /> */}
-    {/* <Header /> */}
-    {/* <Skills/> */}
-    {/* <Quote /> */}
   </React.StrictMode>,
 )
